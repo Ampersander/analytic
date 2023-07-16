2ad0b8068fdbd2f2054ab1030b76646a15ef2cc7
MONDESIR Malik <malikmondesir@gmail.com>
2023-07-16 14:14:14 +0200

    feat: soft deletion for tunnels

--- api/utils/database.js
+++ api/utils/database.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // Configuration de la connexion à la base de données MongoDB
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Connexion à la base de données MongoDB établie avec succès !');
-    } catch (error) {
-        console.error('Erreur lors de la connexion à la base de données MongoDB :', error);
-        process.exit(1); // Arrêter le processus en cas d'erreur de connexion
-    }
-};
+	try {
+		await mongoose.connect(process.env.MONGODB_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		})
+		console.log('Connexion à la base de données MongoDB établie avec succès !')
+	} catch (error) {
+		console.error('Erreur lors de la connexion à la base de données MongoDB :', error)
+		process.exit(1) // Arrêter le processus en cas d'erreur de connexion
+	}
+}
 
-module.exports = { connectDB };
+module.exports = { connectDB }
